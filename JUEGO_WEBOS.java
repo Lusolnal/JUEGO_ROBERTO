af74b5f28b2700972cdd26de86729ab2924a5206
Lusolnal <85316314+Lusolnal@users.noreply.github.com>
2022-03-22 19:42:53 -0600

    Add files via upload

---
+++ JUEGO_WEBOS.java
@@ -0,0 +1,11 @@
+nombre: 
+
+historia del juego: monito que va caminando y le caen muchas cosas del cielo
+
+objetivo: aventar cualquier tipo de cosas
+
+personajes: pc-cosas cayendo y npc: varios monitos
+
+como la visualizamos: entretenido, no apto para niños sencibles
+
+se podra jugar desde la computadora
